974c5fda7ef7e5504bfe3c672174dab5ac756f29
Ariel Mendez <arielmendez735@gmail.com>
2021-01-25 17:58:18 -0600

    update base code to display 3d cube

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 // Author - Ariel Mendez | A01020690
-// El codigo base de este proyecto esta inspirado en
-// el tutorial: https://www.youtube.com/watch?v=kB0ZVUrI4Aw
+// Resouces or tutorials that inspired this project
+// tutorial - https://www.youtube.com/watch?v=kB0ZVUrI4Aw
+// blog - https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
 
-// provides the clip space coordinates
-// attributes get the data from the buffer
-var vertexShaderSrc = [
-  "precision mediump float;",
-  "",
-  "attribute vec2 vertPosition;",
-  "attribute vec3 vertColor;",
-  "varying vec3 fragColor;",
-  "",
-  "void main()",
-  "{",
-  "  fragColor = vertColor;",
-  "  gl_Position = vec4(vertPosition, 0.0, 1.0);",
-  "}",
-].join("\n");
+const createShader = (gl, type, source) => {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+  console.log("Error while creating the shader", gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+};
 
-// provides color
-// fragColor (read, green, blue)
-var fragmentShaderSrc = [
-  "precision mediump float;",
-  "",
-  "varying vec3 fragColor;",
-  "void main()",
-  "{",
-  "  gl_FragColor = vec4(fragColor, 1.0);",
-  "}",
-].join("\n");
+const createProgram = (gl, vertexShader, fragmentShader) => {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+  console.log(
+    "Error while creating the program",
+    gl.getProgramInfoLog(program)
+  );
+  gl.deleteProgram(program);
+};
 
 var main = function () {
   const canvas = document.getElementById("surface");
@@ -45,51 +47,187 @@ var main = function () {
     return;
   }
 
-  // clear color
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // get shaders text
+  var vertexShaderTxt = document.querySelector("#vertex-shader").text;
+  var fragmentShaderTxt = document.querySelector("#fragment-shader").text;
 
-  // create shaders
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderSrc);
-
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderSrc);
-
-  // compile vertex shader
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.error(
-      "ERROR compiling vertex shader!",
-      gl.getShaderInfoLog(vertexShader)
-    );
-    return;
-  }
-
-  // compile fragment shader
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.error(
-      "ERROR compiling fragment shader!",
-      gl.getShaderInfoLog(fragmentShader)
-    );
-    return;
-  }
+  // create the shaders
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderTxt);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderTxt);
 
   // create program
-  var program = gl.createProgram();
+  var program = createProgram(gl, vertexShader, fragmentShader);
 
-  // link the shaders to the program
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error("ERROR linking program!", gl.getProgramInfoLog(program));
-    return;
+  // get vertex attribute locations
+  var positionAttrLocation = gl.getAttribLocation(program, 'vertPosition');
+  var colorAttrLocation = gl.getAttribLocation(program, 'vertColor');
+
+  // get uniform locations
+  var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+  var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+  
+  // create wall vertex buffer
+  var wallVertexBufferObj = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, wallVertexBufferObj);
+  setWallData(gl);
+
+  // create wall index buffer
+  var wallIndexBufferObj = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, wallIndexBufferObj);
+  setWallIndexBuffer(gl);
+
+  drawScene();
+
+  function drawScene() {
+    // clear canvas
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    // enable culling and depth_test
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
+    
+    // Tell OpenGL state machine which program should be active.
+    gl.useProgram(program);
+  
+    // enable position attr
+    gl.enableVertexAttribArray(positionAttrLocation);
+  
+    gl.vertexAttribPointer(
+      positionAttrLocation, // Attribute location
+      3, // Number of elements per attribute
+      gl.FLOAT, // Type of elements
+      gl.FALSE,
+      6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+      0 // Offset from the beginning of a single vertex to this attribute
+    );
+  
+    // enable color attr
+    gl.enableVertexAttribArray(colorAttrLocation);
+  
+    gl.vertexAttribPointer(
+      colorAttrLocation, // Attribute location
+      3, // Number of elements per attribute
+      gl.FLOAT, // Type of elements
+      gl.FALSE,
+      6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+      3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+    );
+  
+    // compute the matrices
+    var worldMatrix = new Float32Array(16);
+    var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(worldMatrix);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
+  
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+  
+    var xRotationMatrix = new Float32Array(16);
+    var yRotationMatrix = new Float32Array(16);
+  
+    //
+    // Main render loop
+    //
+    var identityMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(identityMatrix);
+    var angle = 0;
+    var loop = function () {
+      angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+      glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+      glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+      glMatrix.mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
+      gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+  
+      gl.clearColor(0.75, 0.85, 0.8, 1.0);
+      gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+      gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+  
+      requestAnimationFrame(loop);
+    };
+    requestAnimationFrame(loop);
   }
-  gl.validateProgram(program);
-  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-    console.error("ERROR validating program!", gl.getProgramInfoLog(program));
-    return;
-  }
+
 };
+
+const setWallData = (gl) => {
+
+  var wallVertices = 
+  [ // X, Y, Z           R, G, B
+		// Top
+		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+	];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(wallVertices), gl.STATIC_DRAW);
+}
+
+const setWallIndexBuffer = (gl) => {
+  var boxIndices =
+	[
+		// Top
+		0, 1, 2,
+		0, 2, 3,
+
+		// Left
+		5, 4, 6,
+		6, 4, 7,
+
+		// Right
+		8, 9, 10,
+		8, 10, 11,
+
+		// Front
+		13, 12, 14,
+		15, 14, 12,
+
+		// Back
+		16, 17, 18,
+		16, 18, 19,
+
+		// Bottom
+		21, 20, 22,
+		22, 20, 23
+	];
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
+}
+
+main();
